fc738a7a1f8863d4fd0b49f0d2deb55e13ca45f9
Unknown <ale.bros@hotmail.com>
2017-10-18 15:26:55 -0300

    Super Ultra Mega Push

---
+++ source/Reg.hx
@@ -0,0 +1,12 @@
+package;
+
+/**
+ * ...
+ * @author ...
+ */
+class Reg 
+{	
+	// Player
+	static public var speedX:Int = 130;
+	static public var gravityY:Int = 500;
+}
---
+++ source/entities/Player.hx
@@ -0,0 +1,42 @@
+package entities;
+
+import flixel.FlxSprite;
+import flixel.system.FlxAssets.FlxGraphicAsset;
+import flixel.FlxG;
+
+/**
+ * ...
+ * @author ...
+ */
+enum States
+{
+	MOVE;
+	JUMP;
+	DEAD;
+	DOORTRIGGER;
+}
+class Player extends FlxSprite 
+{
+
+	public function new(?X:Float=0, ?Y:Float=0, ?SimpleGraphic:FlxGraphicAsset) 
+	{
+		super(X, Y, SimpleGraphic);
+		
+	}
+	override public function update(elapsed:Float):Void
+	{
+		super.update(elapsed);
+		
+		movement();
+	}
+	
+	function movement() 
+	{
+		if (FlxG.keys.pressed.RIGHT)	//Right
+			velocity.x += Reg.speedX;
+		if (FlxG.keys.pressed.LEFT)		//Left
+			velocity.x += -Reg.speedX;
+		if (FlxG.keys.justPressed.S)	//Jump
+			velocity.y +=
+	}
+}
