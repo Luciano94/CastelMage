a787b8ed44a6fba983976eee81644d3932945b2f
Unknown <ale.bros@hotmail.com>
2017-10-27 17:44:55 -0300

    2 kind of weapons added

--- source/Reg.hx
+++ source/Reg.hx
@@ -10,7 +10,7 @@ class Reg
 	static public var weaponNormalSpeed:Int = 140;
 	static public var weaponMaxDistance:Int = 90;
 	// Weapon Boomerang
-	static public var weaponBNormalSpeed:Int = 350;
+	static public var weaponBNormalSpeed:Int = 275;
 	static public var weaponYSpeed:Int = -300;
 	// Pause
 	static public var paused:Bool = false;
