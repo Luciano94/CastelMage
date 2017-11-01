354703d173896ca2e0bca26d0b7750ab9257a73c
Unknown <lucho.elp40@gmail.com>
2017-11-01 15:21:19 -0300

    Enemies v1.0

---
+++ source/entities/Enemies/Minion.hx
@@ -0,0 +1,24 @@
+package entities;
+
+import flixel.system.FlxAssets.FlxGraphicAsset;
+
+/**
+ * ...
+ * @author holis
+ */
+class Minion extends Zombie 
+{
+
+	public function new(?X:Float=0, ?Y:Float=0, ?SimpleGraphic:FlxGraphicAsset, _player:Player) 
+	{
+		super(X, Y, SimpleGraphic, _player);
+		loadGraphic(AssetPaths.Minion__png, true, 16, 46);
+		animation.add("idle", [0], false);
+		animation.add("move", [1, 2, 3, 4], 12, true);
+	}
+	
+	override public function reset(X:Float, Y:Float):Void 
+	{
+		super.reset(X, Y);
+	}
+}
